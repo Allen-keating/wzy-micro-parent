77e3b4beb2e926021e9cf77b4caa60c20a4edeb5
allegan <2443491529@qq.com>
2025-12-18 17:19:51 +0800

    first init

--- wzy-client-service/src/main/java/com/wzy/cloud/client/controller/ClientController.java
+++ wzy-client-service/src/main/java/com/wzy/cloud/client/controller/ClientController.java
@@ -2,51 +2,55 @@ package com.wzy.cloud.client.controller;
 
 import com.wzy.cloud.client.entity.Client;
 import com.wzy.cloud.client.service.ClientService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/client") // 修改接口前缀为 /client
+@RequestMapping("/client")
 public class ClientController {
+
+    // 【新增】定义日志对象
+    private static final Logger log = LoggerFactory.getLogger(ClientController.class);
 
     @Autowired
     private ClientService clientService;
 
-    // 1. 查询所有客户 (GET http://localhost:9001/client)
     @RequestMapping(method = RequestMethod.GET)
     public List<Client> findAll() {
         return clientService.findAll();
     }
 
-    // 2. 根据ID查询 (GET http://localhost:9001/client/1)
-
+    // 根据ID查询
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public Client findById(@PathVariable Integer id) {
-        // 添加这一行日志，用于区分实例
+        // 【新增】打印日志，对应教材中的 "进入了UserController的findById方法"
+        log.info("进入了 ClientController 的 findById 方法");
+
+        // 【新增】对应教材中的 System.out.println("用户微服务11111");
+        System.out.println("客户微服务 11111");
+
         System.out.println(">>> 客户服务 [22222] 被调用了！");
         return clientService.findById(id);
     }
 
-
-    // 3. 添加客户 (POST http://localhost:9001/client)
+    // 其他方法保持不变...
     @RequestMapping(method = RequestMethod.POST)
     public String add(@RequestBody Client client) {
         clientService.add(client);
         return "客户添加成功";
     }
 
-    // 4. 修改客户 (PUT http://localhost:9001/client/1)
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
     public String update(@RequestBody Client client, @PathVariable Integer id) {
-        // 确保修改的是路径上指定的ID
         client.setId(id);
         clientService.update(client);
         return "客户信息修改成功";
     }
 
-    // 5. 删除客户 (DELETE http://localhost:9001/client/1)
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public String deleteById(@PathVariable Integer id) {
         clientService.deleteById(id);
--- wzy-order-web/src/main/java/com/wzy/cloud/order/controller/OrderController.java
+++ wzy-order-web/src/main/java/com/wzy/cloud/order/controller/OrderController.java
@@ -2,6 +2,8 @@ package com.wzy.cloud.order.controller;
 
 import com.wzy.cloud.order.client.ClientFeignClient;
 import com.wzy.cloud.order.entity.Client;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,21 +13,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/order")
 public class OrderController {
 
-    // 注入 OpenFeign 接口
+    // 【新增】定义日志对象
+    private static final Logger log = LoggerFactory.getLogger(OrderController.class);
+
     @Autowired
     private ClientFeignClient clientFeignClient;
 
     @RequestMapping(value = "/create", method = RequestMethod.GET)
     public String createOrder() {
-        Integer clientId = 1;
+        // 【新增】打印日志，对应教材中的 log.info("开始调用order方法...")
+        log.info("开始调用 createOrder 方法...");
 
-        // 1. 调用远程服务 (如果服务挂了，会自动进入 ClientFeignClientImpl 的 fallback)
+        Integer clientId = 3;
+
+        // 调用远程服务
         Client client = clientFeignClient.findById(clientId);
 
-        // 2. 打印日志
+        // 打印日志
         System.out.println("==> 收到客户数据: " + client.getClientName());
 
-        // 3. 返回结果
         return "订单创建成功！(OpenFeign+Hystrix)\n" +
                 "所属客户: " + client.getClientName() + "\n" +
                 "备注信息: " + client.getContactPhone();
